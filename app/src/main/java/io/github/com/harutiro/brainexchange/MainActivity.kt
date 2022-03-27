47b378d3574b0116d22ce065a735f8338db96cab
harut <harutiro2027@outlook.jp>
2022-03-27 14:33:24 +0900

    add: Binding を追加

--- app/src/main/java/io/github/com/harutiro/brainexchange/MainActivity.kt
+++ app/src/main/java/io/github/com/harutiro/brainexchange/MainActivity.kt
@@ -2,11 +2,15 @@ package io.github.com.harutiro.brainexchange
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import io.github.com.harutiro.brainexchange.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater).apply { setContentView(this.root) }
 
 
     }
