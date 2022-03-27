1b99bcb3e8b8adf6f020544ee6c1cc3b9e3c8c34
harut <harutiro2027@outlook.jp>
2022-03-28 00:01:15 +0900

    fix: 各アクティビティ　戻る動作をツールバーに表示

--- app/src/main/java/io/github/com/harutiro/brainexchange/NewFlendRegisterActivity.kt
+++ app/src/main/java/io/github/com/harutiro/brainexchange/NewFlendRegisterActivity.kt
@@ -2,10 +2,25 @@ package io.github.com.harutiro.brainexchange
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 
 class NewFlendRegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_new_flend_register)
+
+
+//        戻る→の表示
+        supportActionBar!!.setDisplayShowHomeEnabled(true)
+        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // android.R.id.home に戻るボタンを押した時のidが取得できる
+        if (item.itemId == android.R.id.home) {
+            // 今回はActivityを終了させている
+            finish()
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
