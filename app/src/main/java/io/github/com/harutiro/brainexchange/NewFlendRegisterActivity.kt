769d52256bc2f6678c721810de125b522ca1967e
harut <harutiro2027@outlook.jp>
2022-03-27 14:29:47 +0900

    Initial commit

---
+++ app/src/main/java/io/github/com/harutiro/brainexchange/NewFlendRegisterActivity.kt
@@ -0,0 +1,11 @@
+package io.github.com.harutiro.brainexchange
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class NewFlendRegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_new_flend_register)
+    }
+}
---
+++ app/src/main/java/io/github/com/harutiro/brainexchange/StarterActivity.kt
@@ -0,0 +1,11 @@
+package io.github.com.harutiro.brainexchange
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class StarterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_starter)
+    }
+}
