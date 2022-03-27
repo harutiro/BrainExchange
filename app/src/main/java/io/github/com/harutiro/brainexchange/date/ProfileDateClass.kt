ae0c0e1478a1a976b57a7ea0495b71a8405d6f2e
harut <harutiro2027@outlook.jp>
2022-03-28 01:57:48 +0900

    bug: ProfileDateClass.kt 今までと記述の仕方が変わった

--- app/src/main/java/io/github/com/harutiro/brainexchange/date/ProfileDateClass.kt
+++ app/src/main/java/io/github/com/harutiro/brainexchange/date/ProfileDateClass.kt
@@ -4,11 +4,10 @@ import io.realm.RealmObject
 import io.realm.annotations.PrimaryKey
 import java.util.*
 
-open class ProfileDateClass(
-    @PrimaryKey open var id: String? = UUID.randomUUID().toString(),
-    open var facebookId: String = "",
-    open var brainImageUrl: String = "",
-    open var userName: String = "",
-    open var favList: String = "",
-
-    ): RealmObject
+open class ProfileDateClass: RealmObject{
+    @PrimaryKey open var id: String = ""
+    open var facebookId: String = ""
+    open var brainImageUrl: String = ""
+    open var userName: String = ""
+    open var favList: String = ""
+}
