7d0efb02e99ec86ec95bdd20fa512703cf81a526
harut <harutiro2027@outlook.jp>
2022-03-29 04:59:58 +0900

    fix: StarterActivity.kt 写真を撮れる部分を作った

--- app/src/main/java/io/github/com/harutiro/brainexchange/StarterActivity.kt
+++ app/src/main/java/io/github/com/harutiro/brainexchange/StarterActivity.kt
@@ -1,27 +1,46 @@
 package io.github.com.harutiro.brainexchange
 
+import android.app.Activity
 import android.content.Context
+import android.content.Intent
 import android.content.SharedPreferences
+import android.graphics.Bitmap
+import android.net.Uri
 import android.os.Build
 import android.os.Bundle
+import android.os.Environment
+import android.provider.MediaStore
 import android.util.Log
 import android.view.MenuItem
 import android.view.View.VISIBLE
 import android.widget.Toast
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.ActivityResultCallback
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.activity.result.contract.ActivityResultContracts.StartActivityForResult
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.FileProvider
 import com.google.android.material.chip.Chip
 import com.google.gson.Gson
 import com.google.zxing.BarcodeFormat
 import com.google.zxing.EncodeHintType
-import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel
 import com.journeyapps.barcodescanner.BarcodeEncoder
 import io.github.com.harutiro.brainexchange.databinding.ActivityStarterBinding
 import io.github.com.harutiro.brainexchange.date.ProfileDateClass
+import java.io.File
+import java.text.SimpleDateFormat
+import java.util.*
+
 
 class StarterActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityStarterBinding
+
+    //脳内シート画像
+    lateinit var resitoImage: Bitmap
+    lateinit var currentPhotoUri : Uri
+    var uriString = ""
 
     @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -94,9 +113,47 @@ class StarterActivity : AppCompatActivity() {
             finish()
         }
 
+        binding.starterBrainImage.setOnClickListener {
+            val context: Context = applicationContext
+
+            // 保存先のフォルダー
+            val cFolder: File? = context.getExternalFilesDir(Environment.DIRECTORY_DCIM)
+
+            //        *名前関係*       //
+            //　フォーマット作成
+            val fileDate: String = SimpleDateFormat("ddHHmmss", Locale.US).format(Date())
+            //　名前作成
+            val fileName: String = String.format("CameraIntent_%s.jpg", fileDate)
+
+            //uriの前作成
+            val cameraFile: File = File(cFolder, fileName)
+
+            //uri最終作成
+            currentPhotoUri = FileProvider.getUriForFile(this, context.packageName.toString() + ".fileprovider", cameraFile)
+
+
+            val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            intent.putExtra(MediaStore.EXTRA_OUTPUT, currentPhotoUri)
+            launcher.launch(intent)
+        }
+
 //        戻る→の表示
         supportActionBar!!.setDisplayShowHomeEnabled(true)
         supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+    }
+
+    //    タグインテントにおける戻りデータの受け取り部分
+    private val launcher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){ result: ActivityResult ->
+        if(result.resultCode == Activity.RESULT_OK){
+            val intent = result.data
+
+            binding.starterBrainImage.setImageURI(currentPhotoUri)
+            uriString = currentPhotoUri.toString()
+
+        }else{
+            //メディアプレイヤーに追加したデータを消去する
+            contentResolver.delete(currentPhotoUri, null, null)
+        }
     }
 
     fun setChip(favNumbers: String) {
