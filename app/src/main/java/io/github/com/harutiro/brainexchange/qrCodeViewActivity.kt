5d6a6c9b6386552be82469d05c36944507de916a
harut <harutiro2027@outlook.jp>
2022-03-28 13:38:16 +0900

    add: qrCodeViewActivity.kt QRコードを表示するActivityを作成

---
+++ app/src/main/java/io/github/com/harutiro/brainexchange/qrCodeViewActivity.kt
@@ -0,0 +1,11 @@
+package io.github.com.harutiro.brainexchange
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class qrCodeViewActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_qr_code_view)
+    }
+}
