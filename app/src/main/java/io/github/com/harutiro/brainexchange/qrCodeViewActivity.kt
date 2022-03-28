9eb2b5a28d9c6f0223da4866188cc5357554d862
harut <harutiro2027@outlook.jp>
2022-03-28 16:24:21 +0900

    fix: qrCodeViewActivity.kt QRコードの表示

--- app/src/main/java/io/github/com/harutiro/brainexchange/qrCodeViewActivity.kt
+++ app/src/main/java/io/github/com/harutiro/brainexchange/qrCodeViewActivity.kt
@@ -1,11 +1,48 @@
 package io.github.com.harutiro.brainexchange
 
+import android.content.Context
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.google.gson.Gson
+import com.google.zxing.BarcodeFormat
+import com.google.zxing.EncodeHintType
+import com.journeyapps.barcodescanner.BarcodeEncoder
+import io.github.com.harutiro.brainexchange.databinding.ActivityNewFlendRegisterBinding
+import io.github.com.harutiro.brainexchange.databinding.ActivityQrCodeViewBinding
+import io.github.com.harutiro.brainexchange.databinding.ActivityStarterBinding
+import io.github.com.harutiro.brainexchange.date.ProfileDateClass
 
 class qrCodeViewActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityQrCodeViewBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_qr_code_view)
+        binding = ActivityQrCodeViewBinding.inflate(layoutInflater).apply { setContentView(this.root) }
+
+        val sp: SharedPreferences = getSharedPreferences("DateStore", Context.MODE_PRIVATE)
+
+        // データをバーコードに変更するためのインスタンス
+        val barcodeEncoder = BarcodeEncoder()
+
+        try{
+            // ImageViewにBitmap形式の画像を設定
+            binding.qrCodeViewQrImageView.setImageBitmap(
+                // Bitmap形式でQRコードを生成
+                barcodeEncoder.encodeBitmap(
+                    Gson().toJson(sp.getString("myProfile","")), BarcodeFormat.QR_CODE, 400, 400, mapOf(
+                        EncodeHintType.CHARACTER_SET to "UTF-8"))
+            )
+        } catch (e:Exception){
+            // 生成に失敗したらToastで通知
+            // ex) データが何も無い，エンコード可能なデータ量を超えた，etc...
+            Toast.makeText(this, "Error", Toast.LENGTH_SHORT).show()
+        }
+
+        binding.qrCodeViewFinishButton.setOnClickListener {
+            finish()
+        }
     }
 }
