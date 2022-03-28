7b4837dcede73f8604cb244cc3fab9dc4bf7c644
harut <harutiro2027@outlook.jp>
2022-03-28 15:39:42 +0900

    add: NewFlendRegisterActivity.kt QRコードを読み込んで取り込んだ

--- app/src/main/java/io/github/com/harutiro/brainexchange/NewFlendRegisterActivity.kt
+++ app/src/main/java/io/github/com/harutiro/brainexchange/NewFlendRegisterActivity.kt
@@ -4,20 +4,87 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.MenuItem
+import com.google.android.material.chip.Chip
+import com.google.gson.Gson
+import io.github.com.harutiro.brainexchange.databinding.ActivityMainBinding
+import io.github.com.harutiro.brainexchange.databinding.ActivityNewFlendRegisterBinding
+import io.github.com.harutiro.brainexchange.date.ProfileDateClass
 
 class NewFlendRegisterActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityNewFlendRegisterBinding
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_new_flend_register)
+        binding = ActivityNewFlendRegisterBinding.inflate(layoutInflater).apply { setContentView(this.root) }
+
 
         val getYourProfile = intent.getStringExtra("getYourProfile")
-        
+
+        //        はめ込み部分
+        if(!getYourProfile.isNullOrEmpty()){
+            val getMyProfile: ProfileDateClass = Gson().fromJson(getYourProfile, ProfileDateClass::class.java)
+            binding.newFlendUserNameTextView.text = getMyProfile.userName
+            binding.newFlendFacebookIdTextView.text = getMyProfile.facebookId
+
+            setChip(getMyProfile.favNumbers)
+
+        }else{
+            setChip("")
+        }
 
         Log.d("debag",getYourProfile.toString())
 
 //        戻る→の表示
         supportActionBar!!.setDisplayShowHomeEnabled(true)
         supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+    }
+
+    fun setChip(favNumbers: String) {
+        val favoliteList:List<String> = listOf(
+            "スポーツ",
+            "アニメ",
+            "開発",
+            "ゲーム",
+            "デザイン",
+            "ドラマ",
+            "映画",
+            "音楽",
+            "旅行",
+            "Android",
+            "Unity",
+            "DTM",
+            "iPhone",
+            "WebD",
+            "WebS",
+            "Minecraft",
+            "アニメーション",
+            "メディアアート",
+            "デザイナー",
+            "映像制作",
+            "カメラフォト",
+        )
+
+        for (i in favoliteList) {
+            val chip = Chip(binding.newFlendChipGroup.context)
+            chip.text = i
+
+            // necessary to get single selection working
+            chip.isCheckable = true
+            chip.isClickable = true
+
+            val favLists = favNumbers.split(",")
+            for(j in favLists){
+                if(i == j){
+                    chip.isChecked = true
+                    break
+                }
+            }
+
+            binding.newFlendChipGroup.addView(chip)
+
+        }
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
